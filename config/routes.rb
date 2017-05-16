328204c244dc359d323d77b37c5d9e117df39615
Tasaddaq Altaf <tasaddaq332@gmail.com>
2017-05-16 07:31:12 +0500

    Added routes for views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'fatima_college#index'
+  get "/signup" => "fatima_college#signup"
+  #get 'fatima_college/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
